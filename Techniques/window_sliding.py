4a7cffdbf1305b82c9be1821d41fec356ae72610
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2023-08-19 16:27:29 +0530

    Update

---
+++ Techniques/window_sliding.py
@@ -0,0 +1,2 @@
+#where is use 
+#Array, String, Sub Array, Sub String, Largest Sum, Maximum Sum, Minimum Sum
--- move_neg.py
+++ move_neg.py
@@ -1,18 +1,27 @@
-def rearrange(arr, n ) :
- 
-    # Please refer partition() in 
-    # below post
-    # https://www.geeksforgeeks.org / quick-sort / j = 0
-    j = 0
-    for i in range(0, n) :
-        if (arr[i] < 0) :
-            temp = arr[i]
-            arr[i] = arr[j]
-            arr[j]= temp
-            j = j + 1
-    print(arr)
- 
-# Driver code
-arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9]
-n = len(arr)
-rearrange(arr, n)
+#Two Pointer Approach: The idea is to solve this problem with constant space and 
+#linear time is by using a two-pointer or two-variable approach where we simply 
+# take two variables like left and right which hold the 0 and N-1 indexes. Just need to check that :
+#
+#Check If the left and right pointer elements are negative then simply increment the left pointer.
+#Otherwise, if the left element is positive and the right element is negative then simply swap the elements, and simultaneously increment and decrement the left and right pointers.
+#Else if the left element is positive and the right element is also positive then simply decrement the right pointer.
+#Repeat the above 3 steps until the left pointer ? right pointer.
+
+def segregateElements(self, arr, n):
+    # Your code goes here
+    l = 0
+    r = n-1
+    while l <= r:
+        if arr[l] < 0 and arr[r] < 0:
+            l = l+1
+        elif arr[l] > 0 and arr[r] < 0:
+            arr[l], arr[r] = arr[r], arr[l]
+            l = l+1
+            r = r-1
+        elif arr[l] > 0 and arr[r] > 0:
+            r = r-1
+        else:
+            l = l+1
+            r = r-1
+
+    return arr
