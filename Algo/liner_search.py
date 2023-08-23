f8e9e980ddc17c3634fe6282a3a231193261d7c0
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2023-08-23 13:07:39 +0530

    Initial commit

--- Algo/liner_search.py
+++
@@ -1,9 +0,0 @@
-arr = list(map(int,input().split()))
-n = int(input())
-index = 0
-for i in range(len(arr)):
-    if arr[i]==n:
-        index = i
-        break
-print(index)
-    
--- Techniques/window_sliding.py
+++
@@ -1,2 +0,0 @@
-#where is use 
-#Array, String, Sub Array, Sub String, Largest Sum, Maximum Sum, Minimum Sum
